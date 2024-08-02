a81a4f27c1b162515f3959d21ddc0da51222428a
pedrozvxlindo <151748032+pedrozvxlindo@users.noreply.github.com>
2024-08-02 09:28:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-prinipal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+
+        ]
+    }
+]
