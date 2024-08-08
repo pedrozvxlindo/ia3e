07a15418915b8a0e90b8c8b7e194a0b250e4ecf3
pedrozvxlindo <151748032+pedrozvxlindo@users.noreply.github.com>
2024-08-08 07:44:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const caixaPrincipal = document.querySelector('.caixa-prinipal');
-const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
     {
-        enunciado: "pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-
-        ]
+        enunciado:"Pergunta 1",
+        alternativas:["Alternativa 1", "Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 2",
+        alternativas:["Alternativa 1", "Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 3",
+        alternativas:["Alternativa 1", "Alternativa 2"]
     }
 ]
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+
+}
